9911afea28380db2366b135be4f0099753e4f349
kankeuNde <rodriguekankeu1@gmail.com>
2023-11-28 14:19:35 -0800

    code styling

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 
 for letter in range(ord('a'), ord('z') + 1):
     if letter != ord('q') and letter != ord('e'):
-        print("{:c}".format(letter), end = "")
+        print("{:c}".format(letter), end="")
