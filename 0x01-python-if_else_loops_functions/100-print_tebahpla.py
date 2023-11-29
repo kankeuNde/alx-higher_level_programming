15794742e1c8dbc6802b1c01fb428ec6026b385d
kankeuNde <rodriguekankeu1@gmail.com>
2023-11-28 17:07:41 -0800

    variable name correction

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for letter in range(ord('z'), ord('a') - 1, -1):
-    if (l % 2 != 0):
+    if (letter % 2 != 0):
         case_letter = letter - 32
     else:
         case_letter = letter
