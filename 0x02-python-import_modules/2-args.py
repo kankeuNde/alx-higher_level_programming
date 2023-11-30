7d3ed1095b7d993bdac9da766ac97148d98e3b01
kankeuNde <rodriguekankeu1@gmail.com>
2023-11-30 10:05:31 -0800

    case of 0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ import sys
 if __name__ == "__main__":
     argv_len = len(sys.argv) - 1
     if argv_len == 1:
-        print("{} argument:".format(0))
+        print("1 argument:")
+    elif argv_len == 0:
+        print("0 arguments.")
     else:
         print("{} arguments:".format(argv_len))
     for i in range(1, len(sys.argv)):
