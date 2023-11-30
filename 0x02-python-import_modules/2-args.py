c918ebe0343b13542677fad28b82643890e77efb
kankeuNde <rodriguekankeu1@gmail.com>
2023-11-30 09:54:55 -0800

    formatting fixing

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == "__main__":
     argv_len = len(sys.argv) - 1
     if argv_len == 1:
-        print("1 argument:")
+        print("{} argument:".format(0))
     else:
         print("{} arguments:".format(argv_len))
     for i in range(1, len(sys.argv)):
