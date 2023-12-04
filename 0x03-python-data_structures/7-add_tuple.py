bd9d24797af1fcffd3f4a71c5cdc85a7b2b3f010
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-04 14:23:27 -0800

    Add (0, 0) to each tuple before processing

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,19 +2,9 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a == ():
-        return tuple_b
-    if tuple_b == ():
-        return tuple_a
-    if len(tuple_a) >= 2:
-        a1 = tuple_a[1]
-    else:
-        a1 = 0
-    if len(tuple_b) >= 2:
-        b1 = tuple_b[1]
-    else:
-        b1 = 0
-    tuple_c = (tuple_a[0] + tuple_b[0], a1 + b1)
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+    tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return tuple_c
 
 
