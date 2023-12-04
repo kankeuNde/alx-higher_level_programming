8ecdfdf16f9f38c4c270e08652e0410d5a649a2e
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-04 13:13:55 -0800

    return empty list if list size is 0

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
+    new_list = [None] * len(my_list)
     if len(my_list) > 0:
-        new_list = [None] * len(my_list)
         for i in range(len(my_list)):
             if my_list[i] % 2 == 0:
                 new_list[i] = True
