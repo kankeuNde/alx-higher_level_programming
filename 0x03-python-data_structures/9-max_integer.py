ebbeb61550327a122600aef750d82f282518dcdb
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-04 14:31:04 -0800

    if len(list) is not []

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    if my_list == []:
-        return ("None")
+    if len(my_list) == 0:
+        return "None"
     max = my_list[0]
     for n in my_list:
         if n > max:
