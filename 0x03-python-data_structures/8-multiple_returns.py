b567f1f5f0837fd5954f7e13dec2dc03b36139ed
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-04 12:56:04 -0800

    add main condition

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if not(sentence):
+    if len(sentence) == 0:
         return (0, None)
     str_len = len(sentence)
     first_char = sentence[0]
     return (str_len, first_char)
+
+
+if __name__ == "__main__":
+    multiple_returns
