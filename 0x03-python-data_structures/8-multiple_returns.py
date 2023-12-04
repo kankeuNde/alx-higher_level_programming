49a0644b43d2d0c02d419e598a732d1ce17b5e13
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-04 12:48:55 -0800

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 
 def multiple_returns(sentence):
     if not(sentence):
-        return (len(sentence), None)
+        return (0, None)
     str_len = len(sentence)
     first_char = sentence[0]
     return (str_len, first_char)
