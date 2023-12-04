b20cb3d71e4524abbc93bd1782a58bacfd4e1176
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-04 12:07:41 -0800

    process iff len(list) > 0

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,11 +2,9 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if not my_list == []:
+    if len(my_list) > 0:
         for elt in reversed(my_list):
             print("{:d}".format(elt))
-    else:
-        print()
 
 
 if __name__ == "__main__":
