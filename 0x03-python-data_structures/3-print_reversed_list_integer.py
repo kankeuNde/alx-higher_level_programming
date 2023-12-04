e360713ecaadeb41605b2fdccbca1a9fc05c2674
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-04 14:00:32 -0800

    return if my_list is none

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,11 +2,12 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) > 0:
+    if my_list is None:
+        return
+    else:
         for elt in reversed(my_list):
             print("{:d}".format(elt))
-    else:
-        return
+
 
 if __name__ == "__main__":
     print_reversed_list_integer
