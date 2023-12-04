8ee606368181aee35a787f2de22cfa5ec2ed0192
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-04 12:03:28 -0800

    Empty line if list is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,6 +5,8 @@ def print_reversed_list_integer(my_list=[]):
     if not my_list == []:
         for elt in reversed(my_list):
             print("{:d}".format(elt))
+    else:
+        print()
 
 
 if __name__ == "__main__":
