7d181e9ae8bed3257df413519ab9dc02e93097cd
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-04 13:57:23 -0800

    return in case mylist is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,7 +5,8 @@ def print_reversed_list_integer(my_list=[]):
     if len(my_list) > 0:
         for elt in reversed(my_list):
             print("{:d}".format(elt))
-
+    else:
+        return
 
 if __name__ == "__main__":
     print_reversed_list_integer
