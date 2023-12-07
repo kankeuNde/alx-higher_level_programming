7ccab69cac26828f9feb5d7820731890a1948a35
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-06 22:08:45 -0800

    format the ouput fix

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,7 +3,7 @@
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary:
         for entry in sorted(list(a_dictionary.keys())):
-            print("{}: {}".format(entry, a_dictionary[entry]))
+            print('{}: {}'.format(entry, a_dictionary[entry]))
 
 
 if __name__ == "__main__":
