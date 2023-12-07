007de2ae35399e250b131a00484a35905e8c3899
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-06 18:21:50 -0800

    style fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@
 def square_matrix_simple(matrix=[]):
     squared_matrix = []
     for elt in matrix:
-        squared_matrix.append(list(map(lambda x:x*x, elt)))
+        squared_matrix.append(list(map(lambda x: x*x, elt)))
     return squared_matrix
 
 
