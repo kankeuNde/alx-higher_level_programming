f7e2c2266c8565947c2ed5aebbad4ca7ae0135c7
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-06 18:12:03 -0800

    square a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    squared_matrix = []
+    for elt in matrix:
+        squared_matrix.append(list(map(lambda x:x*x, elt)))
+    return squared_matrix
+
+if __name__ == "__main__":
+    square_matrix_simple
