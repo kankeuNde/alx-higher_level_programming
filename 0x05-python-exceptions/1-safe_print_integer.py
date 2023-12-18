82ce933e212549d7ab874068494424da5bb25bad
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-18 08:25:45 -0800

    print only integer and handle ValueError exception

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    safe_print_integer
