afbe93f17522d93792252fc512c6281cdd69a1ac
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-18 08:12:41 -0800

    count the printed elts

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,13 +3,15 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
+        printed_elts = 0
         for i in range(0, x):
             print(my_list[i], end="")
+            printed_elts += 1
         print()
     except IndexError:
         print()
-        return i
-    return i+1
+        return printed_elts
+    return printed_elts
 
 
 if __name__ == "__main__":
