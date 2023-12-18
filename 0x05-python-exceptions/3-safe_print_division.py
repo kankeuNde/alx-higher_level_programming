e1c784606f5a94b1260c2d6ef0861903beaf55a6
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-18 13:55:43 -0800

    handle any exception, not only Division Error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,12 +2,13 @@
 
 def safe_print_division(a, b):
     try:
-        result = a/b
-    except ZeroDivisionError:
+        result = a / b
+    except:
         result = None
     finally:
         print("Inside result:{}".format(result))
     return result
+
 
 if __name__ == "__main__":
     safe_print_division
