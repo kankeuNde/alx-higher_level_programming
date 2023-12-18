8532eb8b908a248a6145962630bca5cb3c55a94e
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-18 08:02:43 -0800

    print safely

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(0, x):
+            print(my_list[i], end="")
+        print()
+    except IndexError:
+        print()
+        return i
+    return i+1
+
+
+if __name__ == "__main__":
+    safe_print_list
