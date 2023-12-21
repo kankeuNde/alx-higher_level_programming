83e6f92d0d5a8106edf126d7d5b6d722905d4ccd
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-20 21:25:23 -0800

    square 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+    Square class design
+    """
+
+
+class Square:
+    """ Constructor method """
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+
+    """get the size value"""
+    @property
+    def size(self):
+        return (self.__size)
+
+    """set the size value"""
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """ this method compute the square area and return it"""
+    def area(self):
+        return self.__size ** 2
