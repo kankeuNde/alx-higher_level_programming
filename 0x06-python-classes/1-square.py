cc852119169043268d11d06669262c6a854d0c90
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-20 20:24:58 -0800

    code styling

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""" This class is for square creation and management
+    It is an empty class at the moment
+"""
+
 
 class Square:
-
-    def __init__(self, size=None):
+    """ Square class """
+    def __init__(self, size):
         self.__size = size
